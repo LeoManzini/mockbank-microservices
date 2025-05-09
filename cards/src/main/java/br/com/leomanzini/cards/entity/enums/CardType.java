d9a929ecb750ab3502da4142d2377e3a6a14c75c
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-05-09 09:55:30 -0300

    feat: complementing card type enum

--- cards/src/main/java/br/com/leomanzini/cards/entity/enums/CardType.java
+++ cards/src/main/java/br/com/leomanzini/cards/entity/enums/CardType.java
@@ -1,13 +1,21 @@
 package br.com.leomanzini.cards.entity.enums;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
+@Getter
 @Schema(description = "Type of card")
 public enum CardType {
     @Schema(description = "Credit card")
-    CREDIT,
+    CREDIT("Credit"),
     @Schema(description = "Debit card")
-    DEBIT,
+    DEBIT("Debit"),
     @Schema(description = "Prepaid card")
-    PREPAID;
+    PREPAID("Prepaid");
+
+    private final String type;
+
+    CardType(String type) {
+        this.type = type;
+    }
 }
