1f69ae1b3bee05541489327a1b7dad12687b619f
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-05-09 10:09:24 -0300

    feat: building utils

---
+++ loans/src/main/java/br/com/leomanzini/loans/utils/LoansConstants.java
@@ -0,0 +1,12 @@
+package br.com.leomanzini.loans.utils;
+
+public final class LoansConstants {
+
+    private LoansConstants() {
+        // restrict instantiation
+    }
+
+    public static final int  NEW_LOAN_LIMIT = 1_00_000;
+    public static final String  MESSAGE_201 = "Loan created successfully";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+}
