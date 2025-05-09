5a05d436cce75a35b08a4d15aae09c57b3f54f96
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-05-09 11:14:11 -0300

    feat: enum with methods

--- accounts/src/main/java/br/com/leomanzini/accounts/entity/enums/AccountType.java
+++ accounts/src/main/java/br/com/leomanzini/accounts/entity/enums/AccountType.java
@@ -1,11 +1,19 @@
 package br.com.leomanzini.accounts.entity.enums;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
+@Getter
 @Schema(description = "Type of bank account")
 public enum AccountType {
     @Schema(description = "Savings account")
-    SAVINGS,
+    SAVINGS("Savings"),
     @Schema(description = "Checking account")
-    CHECKING;
+    CHECKING("Checking");
+
+    private final String type;
+
+    AccountType(String type) {
+        this.type = type;
+    }
 }
