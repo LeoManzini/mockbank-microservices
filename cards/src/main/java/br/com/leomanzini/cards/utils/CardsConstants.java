b7aa47210c0c274547063b67303d5310a7daeec6
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-05-09 09:01:16 -0300

    feat: adding aux packages

---
+++ cards/src/main/java/br/com/leomanzini/cards/utils/CardsConstants.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.cards.utils;
+
+public final class CardsConstants {
+
+    private CardsConstants() {
+        // Prevent instantiation
+    }
+
+    public static final String  CREDIT_CARD = "Credit Card";
+    public static final int     NEW_CARD_LIMIT = 1_00_000;
+    public static final String  MESSAGE_201 = "Card created successfully";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+}
