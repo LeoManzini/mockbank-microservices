225acc2360fa6e64b775dff37a635449490b9897
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-08-13 19:53:47 -0300

    feat: accounts auto refresh without API restart

--- accounts/src/main/java/br/com/leomanzini/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/br/com/leomanzini/accounts/dto/AccountsContactInfoDto.java
@@ -1,13 +1,16 @@
 package br.com.leomanzini.accounts.dto;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) { }
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
